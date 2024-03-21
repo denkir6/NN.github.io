bf3d6812ee1e1c5a570340d74b0d89855a1eb081
denkir6 <denkir6@gmail.com>
2024-03-21 09:58:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function fillLabel(){
 		}
 	//var data = ctx.getImageData(1,1,1,1);
 	//console.log(data);
-	console.log("Lt");/*
+	console.log("Lst");/*
 
 	for (var i = 0; i < result.length; i++){
 		result[i] = 253;	
